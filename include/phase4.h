2798df53eca3b98e37254394aba47286308f0d11
limxx518 <janwei25@gmail.com>
2019-11-18 13:03:39 -0600

    Deleted include folder

--- include/phase4.h
+++
@@ -1,7 +0,0 @@
-// Add Guard to header file
-#ifndef PHASE4_H
-#define PHASE4_H
-// Function prototypes for
-//			Master process's reporting procedure
-int phase4(void);	//Function copies results from ReducerResult.txt and writes it to FinalResult.txt using output redirection
-#endif
