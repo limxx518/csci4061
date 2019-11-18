2f1f8cc1c1d96a555144505a1d6d5b10a9c4bb64
limxx518 <janwei25@gmail.com>
2019-11-18 13:22:01 -0600

    Revert "Completed code for PA2"
    This reverts commit 7afe8c20af9c33fc5b6f6809f7ec4af7a89e829d.

--- pa2/include/phase2_3.h
+++
@@ -1,8 +0,0 @@
-#ifndef PHASE2_3_H
-#define PHASE2_3_H
-
-//Function prototypes for mapper and reduce processes
-int mapper(char *str, int mapper_id, int fd[][2]);	//Function used by each mapper process to count number of letters corresponding to the words in the text files
-int phase2_3(int num_fork);	//Wrapper for mapper function
-
-#endif
